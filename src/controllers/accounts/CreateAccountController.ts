3a9ab1179cfc2fdf6737ce1e561e9a9be9253ebf
Pedro Augusto <pedro007augustobarbosa@gmail.com>
2022-05-12 22:40:32 -0300

    Configuring prisma, create models and init first route

---
+++ src/controllers/accounts/CreateAccountController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+import prisma from "../prismaProvider"; 
+
+const CreateAccountController = {
+    create: async function (req: Request, resp: Response) {
+
+
+    }
+
+} 
+
+export default CreateAccountController;
--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,15 @@
 import express, { Router } from "express";
+import CreateAccountController from "./controllers/accounts/CreateAccountController";
 
 const routes = Router();
 
 routes.get("/", (request: express.Request, response: express.Response) => {
     response.json({
         msg: "Hello World",
-    })
+    });
 
 });
+
+routes.post("/account/", CreateAccountController.create);
 
 export default routes;
